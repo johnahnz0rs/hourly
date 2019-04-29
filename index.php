8ad4995121ec7e2957ec0d18cd94708ea62ad8d6
John Ahn <john@launchpartner.co>
2019-04-29 10:21:58 -0700

    first commit; my first php project

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php 
+	$activities = ['foam roll', 'sun salutation(s)', 'visit whole foods', 'walk around the block'];
+	$random = rand(0, (count($activities) -1));
+	$suggestion = $activities[$random];
+?>
+
+<!doctype html>
+<html lang="en">
+  	<head>
+	    <!-- Required meta tags -->
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	    <title>Hourly | z0rs</title>
+  	</head>
+  <body>
+  	<div class="container text-center">
+	    <div class="row" id="header">
+	    	<div class="col">
+	    		<h1>Hello, world!</h1>
+	    	</div>
+	    </div>
+
+	    <div class="row" id="form-what-do">
+	    	<div class="col">
+			    <form action="index.php" method="POST">
+
+			    	<input type="submit" name="submit" value="whatDo?" class="btn btn-lg btn-success">
+
+			    </form>
+			</div>
+		</div>
+
+		<div class="row" id="suggestion-what-do">
+			<div class="col">
+				<p>
+				<?php 
+					if(isset($_POST['submit'])) {
+						echo $suggestion;
+					}
+				?>
+			</div>
+		</div>
+	</div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
